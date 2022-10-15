9e9241a7a1866d1d63b51e2b1bacaaa0ed013761
wisamokkeh <wisamokkeh2@gmail.com>
2022-10-15 14:56:19 +0300

    sdc

---
+++ Requestssss/src/testFet.py
@@ -0,0 +1,21 @@
+import allure
+from ddt import *
+from unittest import TestCase
+
+from request.getRequest import dataFromWeb
+from Core.WorkWithCsv import WorkWithCsv
+
+
+@ddt
+class TestData(TestCase):
+    def setUp(self):
+         allure.attach.file("../files/testData.csv","test_data.csv",attachment_type=allure.attachment_type.CSV)
+
+    @data(*WorkWithCsv.get_csv_data("../files/testData.csv"))
+    @unpack
+    def testRequest(self,Route , StatusCode):
+        statuscode = dataFromWeb.getData(self , Route)
+        self.assertEqual(str(StatusCode) , str(statuscode))
+
+
+
